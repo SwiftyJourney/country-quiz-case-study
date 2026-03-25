e09fc16e8cc8dee1f379a2fed66c4af45771f5a5
Juan Dorado <jfdoradotr@icloud.com>
2026-03-24 18:48:01 -0600

    Create swift package with CountryQuiz shared library target

---
+++ CountryQuiz/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+  name: "CountryQuiz",
+  products: [
+    .library(
+      name: "CountryQuiz",
+      targets: ["CountryQuiz"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "CountryQuiz"
+    ),
+  ]
+)
