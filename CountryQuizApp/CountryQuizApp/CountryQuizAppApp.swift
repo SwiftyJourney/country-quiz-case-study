0910f8e614e80a79765ed35111bf7049f7c91bf2
Juan Dorado <jfdoradotr@icloud.com>
2026-03-26 09:41:15 -0600

    Create the empty skeleton country quiz app

---
+++ CountryQuizApp/CountryQuizApp/CountryQuizAppApp.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CountryQuizAppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
