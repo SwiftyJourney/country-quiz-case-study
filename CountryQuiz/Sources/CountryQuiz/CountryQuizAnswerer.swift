996695fd90c890d27395f2aae7d55a81a6550fcb
Juan Dorado <jfdoradotr@icloud.com>
2026-03-26 09:38:42 -0600

    use `localized` string comparison for case-insensitive country name matching

--- CountryQuiz/Sources/CountryQuiz/CountryQuizAnswerer.swift
+++ CountryQuiz/Sources/CountryQuiz/CountryQuizAnswerer.swift
@@ -6,20 +6,20 @@ public struct CountryQuizAnswerer {
   public init() {}
 
   public func capital(for countryName: String, in countries: [Country]) -> String? {
-    countries.first { $0.name.lowercased() == countryName.lowercased() }?.capital
+    countries.first { $0.name.localizedCaseInsensitiveContains(countryName) }?.capital
   }
 
   public func countries(startingWith prefix: String, in countries: [Country]) -> [String] {
     countries
-      .filter { $0.name.lowercased().hasPrefix(prefix.lowercased()) }
+      .filter { $0.name.range(of: prefix, options: [.caseInsensitive, .anchored]) != nil }
       .map(\.name)
   }
 
   public func code(for countryName: String, countries: [Country]) -> String? {
-    countries.first { $0.name.lowercased() == countryName.lowercased() }?.code
+    countries.first { $0.name.localizedCaseInsensitiveContains(countryName) }?.code
   }
 
   public func flag(for countryName: String, countries: [Country]) -> String? {
-    countries.first { $0.name.lowercased() == countryName.lowercased() }?.flag
+    countries.first { $0.name.localizedCaseInsensitiveContains(countryName) }?.flag
   }
 }
