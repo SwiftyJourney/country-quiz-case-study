0e2e1a4f4b252ec6680de50bf8434c753c1fee7c
Juan Dorado <jfdoradotr@icloud.com>
2026-03-26 20:31:25 -0600

    Compose the app on the main app

--- CountryQuizApp/CountryQuizApp/CountryQuizAppApp.swift
+++ CountryQuizApp/CountryQuizApp/CountryQuizAppApp.swift
@@ -2,13 +2,31 @@
 //  Copyright © Juan Francisco Dorado Torres. All rights reserved.
 //
 
+import CountryQuiz
+import CountryQuizAPI
+import CountryQuizFoundationModels
+import CountryQuiziOS
 import SwiftUI
 
 @main
 struct CountryQuizAppApp: App {
+  private let viewModel: ChatViewModel
+
+  init() {
+    let url = URL(string: "https://restcountries.com/v3.1/all?fields=name,capital,cca2,flag")!
+    let client = URLSessionHTTPClient()
+    let loader = RemoteCountryLoader(url: url, client: client)
+    let interpreter = FoundationModelsQuestionInterpreter()
+    let service = QuizService(interpreter: interpreter, loader: loader)
+
+    self.viewModel = ChatViewModel(answerQuestion: service.answer)
+  }
+
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      NavigationStack {
+        ChatView(viewModel: viewModel)
+      }
     }
   }
 }
